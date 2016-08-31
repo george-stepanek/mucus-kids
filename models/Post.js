f0e9b7de4d95311dbd5a712af8d866f787002009
stepang <g.stepanek.nz@gmail.com>
2016-08-31 22:25:52 +0000

    Update blog layout

--- models/Post.js
+++ models/Post.js
@@ -17,10 +17,7 @@ Post.add({
 	author: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.CloudinaryImage },
-	content: {
-		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-		extended: { type: Types.Html, wysiwyg: true, height: 400 },
-	},
+	content: { type: Types.Html, wysiwyg: true, height: 400 },
 });
 
 Post.schema.virtual('content.full').get(function () {
--- routes/views/blog.js
+++ routes/views/blog.js
@@ -18,7 +18,7 @@ exports = module.exports = function (req, res) {
 
 		var q = keystone.list('Post').paginate({
 			page: req.query.page || 1,
-			perPage: 10,
+			perPage: 1,
 			maxPages: 10,
 			filters: {
 				state: 'published',
